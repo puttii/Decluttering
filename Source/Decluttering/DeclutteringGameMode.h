2cbff61ace5bde7943c43c9f8fdf59d15c2f4116
xuanzhi liu <1481069264@qq.com>
2026-05-12 20:44:58 +0800

    first commit

---
+++ Source/Decluttering/DeclutteringGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DeclutteringGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ADeclutteringGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ADeclutteringGameMode();
+};
+
+
+
